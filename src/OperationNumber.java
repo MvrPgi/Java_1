2f9a04d2edd74ff3846006d85177c3a6db1a2067
Mover <movermercene@gmail.com>
2024-04-07 19:11:21 +0800

    ACTIVITY_DONE

---
+++ src/OperationNumber.java
@@ -0,0 +1,16 @@
+public class OperationNumber {
+  @SuppressWarnings("unused")
+  public static void main(String[] args) {
+  
+      boolean R1 = (!((2*5+3)-10!=5) || (20/5+15) >=56 && 10!=3+10);
+      System.err.println(R1);
+
+
+      boolean R2 = (!((2*5+3)-10!=5) || (20/5+15) >=56 && 10!=3+10);
+      System.err.println(R2);
+
+    
+      boolean R3 = (!((2*5+3)+10==15 && (5%5+2) >=5) || 10<3%2);
+      System.err.println(R3);
+  }
+}
