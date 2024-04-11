9c6d1241d667dbd1d791babdce19acc5076dbf3a
Mover <movermercene@gmail.com>
2024-04-12 07:18:50 +0800

    11/04/24

--- LESSON/JAVA_L6PRC_2.java
+++ LESSON/JAVA_L6PRC_2.java
@@ -1,6 +1,11 @@
-//import java.util.Random;
+import java.util.Random;
 public class JAVA_L6PRC_2 {
   public static void main(String[] args) {
+    Random random = new Random();
+    //double y = random.nextDouble();
+    //int x = random.nextInt(6)+1;
+    boolean z = random.nextBoolean();
+    System.out.println(z);
     
   }
 }
